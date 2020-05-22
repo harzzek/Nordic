f7ef027f1288cc6b74fd96ccc9924c061700396e
DookmanGit <jonasnbp@gmail.com>
2020-05-22 11:34:54 +0200

    Booking

---
+++ Nordic Project/src/main/java/com/example/demo/Model/Booking.java
@@ -0,0 +1,18 @@
+package com.example.demo.Model;
+
+import java.time.LocalDate;
+
+public class Booking
+{
+    int idBooking;
+    LocalDate bookingDate;
+    String pickup;
+    String dropoff;
+    int idCustomer;
+    int idMotorhome;
+
+    public void Booking(LocalDate bookingDate, String pickup, String dropoff, int idCustomer, int idMotorhome)
+    {
+
+    }
+}
