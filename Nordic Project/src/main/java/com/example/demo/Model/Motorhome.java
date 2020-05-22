789bdc88a04212edd85ff5353133853b999077c6
benjizx <64012546+benjizx@users.noreply.github.com>
2020-05-22 11:55:16 +0200

    added index and mountains and motorhome picture

--- Nordic Project/src/main/java/com/example/demo/Model/Motorhome.java
+++ Nordic Project/src/main/java/com/example/demo/Model/Motorhome.java
@@ -9,6 +9,47 @@ public class Motorhome
     int size;
     boolean status;
 
-    public void Motorhome(int id)
+    public Motorhome(int idMotorhome, String type, String brand, String model, int size, boolean status){
+        this.idMotorhome = idMotorhome;
+        this.type = type;
+        this.brand = brand;
+        this.model = model;
+        this.size = size;
+        this.status = status;
+    }
 
+    public int getIdMotorhome()
+    {
+        return idMotorhome;
+    }
+
+    public void setIdMotorhome(int idMotorhome)
+    {
+        this.idMotorhome = idMotorhome;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public String getBrand()
+    {
+        return brand;
+    }
+
+    public String getModel()
+    {
+        return model;
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public boolean isStatus()
+    {
+        return status;
+    }
 }
