f70fb779057263fda9fd031d893f14a22f20f265
benjizx <64012546+benjizx@users.noreply.github.com>
2020-05-22 11:48:57 +0200

    added index and mountains and motorhome picture

---
+++ Nordic Project/src/main/java/com/example/demo/Model/Motorhome.java
@@ -0,0 +1,14 @@
+package com.example.demo.Model;
+
+public class Motorhome
+{
+    int idMotorhome;
+    String type;
+    String brand;
+    String model;
+    int size;
+    boolean status;
+
+    public void Motorhome(int id)
+
+}
