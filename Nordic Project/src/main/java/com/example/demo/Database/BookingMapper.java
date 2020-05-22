494b43c85c8656e3bd20e4b3e2d948dc6aa2f916
harzzek <39692510+harzzek@users.noreply.github.com>
2020-05-22 12:15:24 +0200

    Packages with the homecontroller and index satisfied

---
+++ Nordic Project/src/main/java/com/example/demo/Database/BookingMapper.java
@@ -0,0 +1,31 @@
+package com.example.demo.Database;
+
+import java.util.ArrayList;
+
+public class BookingMapper
+{
+    public void createBooking(Motorhome motorhome, Customer customer)
+    {
+
+    }
+
+    public void deleteBooking(int phoneNr)
+    {
+
+    }
+
+    public void updateBooking(Booking booking)
+    {
+
+    }
+
+    public ArrayList<Booking> listOfBookings()
+    {
+
+    }
+
+    public Booking findBooking(int phoneNr)
+    {
+
+    }
+}
